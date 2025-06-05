7ea8c9ea9c5a0474e4217d27e0c8d60a042bc892
irtaza <arsal.h@yahoo.com>
2025-06-06 02:44:44 +0500

    adding footer and home page design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,60 +1,86 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel=" stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
-    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+
+    {{-- jquery --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"
+        integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    {{-- jQy --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-    <!-- Add Select2 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <!-- Add Select2 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    {{-- Montesserat Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
 
-        <!-- Fonts -->
+    {{-- moul font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Montagu+Slab:opsz,wght@16..144,100..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Moul&family=Poppins:ital,wght@0,100..900;1,100..900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Montagu+Slab:opsz,wght@16..144,100..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Moul&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
-    <!-- Alpine.js -->
+    <!-- Include Alpine.js in your layout if not already included -->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
+    {{-- dm sans right pt --}}
+    {{-- <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet"> --}}
 
-    <!-- Slick Carousel -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    {{-- montagu slab font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montagu+Slab:opsz,wght@16..144,100..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
 
+        <!-- Add Select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- Add Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Fonts -->
+    <title>TFSPL</title>
+    <style>
+        .box {
+            background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='25' ry='25' stroke='black' stroke-width='4' stroke-dasharray='6%2c 12' stroke-dashoffset='2' stroke-linecap='square'/%3e%3c/svg%3e");
+            border-radius: 25px;
+        }
+    </style>
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
+<body class="font-montserrat w-full">
 
-        <!-- Page Heading -->
-        @isset($header)
+    {{-- @include('partials.header') --}}
+    <div>
+     @include('layouts.navigation')
+
+        {{-- @yield('content') --}}
+         @isset($header)
             <header class="bg-white dark:bg-gray-800 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
@@ -66,6 +92,9 @@
             {{ $slot }}
         </main>
     </div>
+
+    @include('partials.footer')
+
 </body>
 
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,116 @@
+<footer class="relative">
+    <img src="{{ asset('storage/new/fbg.png') }}" alt=""
+        class="absolute top-0 left-0 h-full object-cover object-center w-full">
+
+    <div class="relative">
+        <div class="w-[90%] md:w-[84%] mx-auto 2xl:px-[80px] xl:px-[60px] lg:px-[40px]">
+
+            <!-- Main Footer Content -->
+            <div
+                class="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-7 items-center pt-[30px] pb-[25px] text-left gap-4 md:gap-6 lg:gap-[50px]">
+
+                <!-- Logo -->
+                <div class="md:col-span-2 lg:col-span-3 flex justify-center md:justify-start ">
+                    <h2
+                        class="text-[22px] md:text-[25px] lg:text-[27px] font-montserrat font-bold text-white w-full md:w-[300px] lg:w-[319px] h-auto ">
+                        TWENTY FOUR SEVEN PREMIER LEAGUE<span class="text-[#2097D9]">.</span>
+                    </h2>
+                </div>
+
+                <!-- Navigation Links -->
+                <div
+                    class="md:col-span-2 lg:col-span-4 flex flex-wrap md:flex-nowrap justify-between gap-4 md:gap-[20px] lg:gap-[20px] xl:gap-[60px] 2xl:gap-[110px]">
+                    <div>
+                        <h3 class="font-bold mb-4 text-white font-montserrat text-[15px] md:text-[16px]">ABOUT</h3>
+                        <ul
+                            class="space-y-2 md:space-y-3 text-[11px] md:text-[12px] font-montserrat font-medium text-white uppercase">
+                            <li>News</li>
+                            <li>Sponsors</li>
+                            <li>Teams</li>
+                            <li>Players</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 class="font-bold mb-4 text-white font-montserrat text-[15px] md:text-[16px]">MATCH</h3>
+                        <ul
+                            class="space-y-2 md:space-y-3 text-[11px] md:text-[12px] font-montserrat font-normal text-white uppercase">
+                            <li>Schedule</li>
+                            <li>Results</li>
+                            <li>Ranking</li>
+                            <li>Standings</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 class="font-bold mb-4 text-white font-montserrat text-[15px] md:text-[16px]">GALLERY</h3>
+                        <ul
+                            class="space-y-2 md:space-y-3 text-[11px] md:text-[12px] font-montserrat font-normal text-white uppercase">
+                            <li>Videos</li>
+                            <li>Photos</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 class="font-bold mb-4 text-white font-montserrat text-[15px] md:text-[16px]">MORE</h3>
+                        <ul
+                            class="space-y-2 md:space-y-3 text-[11px] md:text-[12px] font-montserrat font-normal text-white uppercase">
+                            <li>Email</li>
+                            <li>Contacts</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Divider -->
+        <div class="border-t border-gray-500  mx-[80px] "></div>
+
+        <div class="w-[90%] md:w-[84%] mx-auto 2xl:px-[80px] xl:px-[60px] lg:px-[40px] py-[12px] md:py-[16px]">
+            <!-- Footer Bottom Section -->
+            <div
+                class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-7 items-center text-sm text-gray-400 text-center sm:text-left gap-4 md:gap-6 lg:gap-[50px]">
+
+                <!-- Copyright -->
+                <div class="sm:col-span-2 lg:col-span-3">
+                    <p class="text-[11px] md:text-[12px] font-montserrat font-normal">
+                        COPYRIGHT &copy; 2024 TWENTY FOUR SEVEN PREMIER LEAGUE. ALL RIGHTS RESERVED.
+                    </p>
+                </div>
+
+                <div
+                    class="sm:col-span-2 lg:col-span-4 flex flex-col sm:flex-row justify-between items-center gap-3 md:gap-4 lg:gap-6">
+
+                    <!-- Social Media Icons -->
+                    <div class="flex gap-3 md:gap-4 lg:gap-[23px]">
+                        <a href="#"
+                            class="flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full border bg-white border-[#094AB7] text-[#094AB7] text-lg md:text-xl  transition-all duration-300">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#"
+                            class="flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full border bg-white border-[#094AB7] text-[#094AB7] text-lg md:text-xl  transition-all duration-300">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="#"
+                            class="flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full border bg-white border-[#094AB7] text-[#094AB7] text-lg md:text-xl  transition-all duration-300">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+
+                    <!-- Back to Top Button -->
+                    <div class="sm:flex sm:justify-end ">
+                        <button onclick="window.scrollTo({ top: 0, behavior: 'smooth' })" class="cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" md:width="34"
+                                md:height="34" viewBox="0 0 34 34" fill="none">
+                                <circle cx="17" cy="17" r="17" fill="#010724" />
+                                <path d="M23 16L17 11M17 11L11 16M17 11L17 23" stroke="white" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
