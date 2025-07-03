5ee1478c0765bad3176008aa0362cbc6cbd8ad8c
irtaza <arsal.h@yahoo.com>
2025-07-03 19:42:45 +0500

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,6 +32,14 @@ class DatabaseSeeder extends Seeder
             ['name' => 'spectator', 'description' => 'Spectator', 'guard_name' => 'web'],
         ]);
 
+        // Create admin user
+        $admin = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('test@123'),
+        ]);
+        $admin->roles()->attach($roles->where('name', 'admin')->first()->id);
+
         $users = User::factory(20)->create()->each(function ($user) use ($roles) {
             $user->roles()->attach(
                 $roles->random(rand(1, 2))->pluck('id')
@@ -60,7 +68,6 @@ class DatabaseSeeder extends Seeder
         });
 
         $teams->each(function ($team) use ($players) {
-            // $selectedPlayers = $players->random(rand(3, 7));
             $selectedPlayers = $players->shuffle()->take(min($players->count(), rand(3, 7)));
 
             $selectedPlayers->each(function ($player, $index) use ($team) {
@@ -110,6 +117,65 @@ class DatabaseSeeder extends Seeder
             }
         });
 
+        // Add blog posts
+        DB::table('blogs')->insert([
+            [
+                'title' => 'Wolve Winning Celebration',
+                'slug' => 'wolve-winning-celeberations',
+                'thumbnail' => 'blog-thumbnails/SbR5G3UDL45Mxmgn1AbpO4OFvz64fNjft8iIdOmn.png',
+                'scripts' => NULL,
+                'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop published.',
+                'published_at' => '2025-06-12 21:42:00',
+                'created_at' => '2025-06-03 16:42:36',
+                'updated_at' => '2025-06-03 19:07:59',
+                'deleted_at' => NULL
+            ],
+            [
+                'title' => 'Cricket Tournament Announcement',
+                'slug' => 'cricket-tournament-announcement',
+                'thumbnail' => 'blog-thumbnails/tournament-announcement.png',
+                'scripts' => NULL,
+                'content' => 'The annual cricket tournament is just around the corner! Teams from all over the region will compete for the prestigious trophy. Stay tuned for updates on fixtures, player performances, and exciting match highlights throughout the season.',
+                'published_at' => '2025-06-15 10:00:00',
+                'created_at' => '2025-06-10 09:15:22',
+                'updated_at' => '2025-06-10 09:15:22',
+                'deleted_at' => NULL
+            ],
+            [
+                'title' => 'Player of the Month: Interview',
+                'slug' => 'player-of-the-month-interview',
+                'thumbnail' => 'blog-thumbnails/player-interview.png',
+                'scripts' => NULL,
+                'content' => 'We sat down with this month\'s standout player to discuss their training regimen, match preparation, and what motivates them to perform at their best. Learn the secrets behind their consistent performances and what they eat before a big match!',
+                'published_at' => '2025-06-20 14:30:00',
+                'created_at' => '2025-06-15 11:45:18',
+                'updated_at' => '2025-06-18 16:20:05',
+                'deleted_at' => NULL
+            ],
+            [
+                'title' => 'Top 5 Cricket Strategies',
+                'slug' => 'top-5-cricket-strategies',
+                'thumbnail' => 'blog-thumbnails/cricket-strategies.png',
+                'scripts' => NULL,
+                'content' => 'Discover the top 5 strategies employed by winning teams this season. From innovative field placements to clever bowling changes, we break down the tactics that are making a difference in close matches.',
+                'published_at' => '2025-06-25 08:00:00',
+                'created_at' => '2025-06-20 13:10:33',
+                'updated_at' => '2025-06-22 10:15:47',
+                'deleted_at' => NULL
+            ],
+            [
+                'title' => 'Upcoming Youth Cricket Camp',
+                'slug' => 'upcoming-youth-cricket-camp',
+                'thumbnail' => 'blog-thumbnails/youth-camp.png',
+                'scripts' => NULL,
+                'content' => 'Registration is now open for our summer youth cricket camp! Young players aged 10-16 can learn from professional coaches and former players. Limited spots available - don\'t miss this opportunity to improve your game!',
+                'published_at' => '2025-07-01 09:00:00',
+                'created_at' => '2025-06-25 14:20:10',
+                'updated_at' => '2025-06-28 11:05:29',
+                'deleted_at' => NULL
+            ]
+        ]);
+
         // Re-enable foreign key constraints
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
@@ -117,6 +183,7 @@ class DatabaseSeeder extends Seeder
     protected function truncateTables()
     {
         $tables = [
+            'blogs',
             'batting_scores',
             'bowling_figures',
             'innings',
